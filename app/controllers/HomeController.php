4dc47c046ddae19572bb9cf3284fc459db08fc20
foysal <cfaion341@gmail.com>
2013-12-24 00:52:42 +0100

    init commit

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * HomeController
+ */
+class HomeController extends BaseController {
+	
+}
---
+++ gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'public/css/main.css' : 'app/sass/main.scss',
+          'public/css/plugins.css' : 'app/sass/plugins.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['app/sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        }
+      },
+      js: {
+        files: ['app/scripts/**/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    concat: {
+      main: {
+        src: [
+        ],
+        dest: 'public/js/main.js'
+      }
+    },
+    uglify: {
+      main: {
+            files: { 
+                'public/js/plugins.min.js': ['public/js/plugins.js'],
+                'public/js/main.min.js': ['public/js/main.js']
+            }
+      }
+      },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default',['watch']);
+}
